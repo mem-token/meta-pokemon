5e4ef78447b70a1b92a7376630d8bad50baf6e99
mem-token <119497259+mem-token@users.noreply.github.com>
2022-12-10 11:54:37 +0300

    Update deploy_with_ethers.ts

--- scripts/deploy_with_ethers.ts
+++ scripts/deploy_with_ethers.ts
@@ -2,7 +2,7 @@ import { deploy } from './ethers-lib'
 
 (async () => {
     try {
-        const result = await deploy('MyToken', ['0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3','0x73c38e1498102Cd42E402511455e6F95F8Dd1606','0x73c38e1498102Cd42E402511455e6F95F8Dd1606'])
+        const result = await deploy('MyToken', ['0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3','0xA82bC8D792E20B74E0Dd81555b7b741A0cb13440','0xA82bC8D792E20B74E0Dd81555b7b741A0cb13440'])
         console.log(`address: ${result.address}`)
     } catch (e) {
         console.log(e.message)
